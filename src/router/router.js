bcae078fbd573577f05b16e27e8e6bf089258ae5
chunkitmax <chunkitmax1125@gmail.com>
2018-08-05 23:57:27 +0800

    Remove old comments

--- src/router/router.js
+++ src/router/router.js
@@ -3,8 +3,6 @@ import Router from 'vue-router'
 
 import Loader from '../components/Loader.vue'
 import MainConsole from '../components/MainConsole.vue'
-// import Home from '../components/Home.vue';
-// import About from '../components/About.vue';
 
 Vue.use(Router)
 
@@ -15,8 +13,6 @@ export function createRouter () {
     routes: [
       { path: '/', component: Loader },
       { path: '/main', component: MainConsole }
-      // { path: '/', component: Home },
-      // { path: '/about', component: About }
     ]
   })
 }
