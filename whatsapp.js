4d4237bed140fc436c4fa5d94f3392b905c95c49
chunkitmax <chunkitmax1125@gmail.com>
2018-08-11 00:52:47 +0800

    online statistics

--- whatsapp.js
+++ whatsapp.js
@@ -30,7 +30,7 @@ class Tab {
       './div/div[position()=2]/div[position()=1]/div[position()=2]/span'
     )).getText()
     try {
-      this.picUrl = await elem.findElement(By.tagName('img')).getAttribute('src')
+      this.picUrl = '/icon?e='+encodeURIComponent(await elem.findElement(By.tagName('img')).getAttribute('src'))
     } catch (e) { }
     let statusElem = null
     try {
@@ -63,6 +63,10 @@ class WhatsApp {
       width: 640,
       height: 480
     };
+    // let profile = new firefox.Profile()
+    // profile.setPreference('security.csp.enable', false)
+    // profile.setPreference('security.mixed_content.block_active_content', false)
+    // profile.DEFAULT_PREFERENCES['frozen']["security.csp.enable"] = False
     let webDriverBuilder = new Webdriver.Builder()
     this.browser = (() => {
       let tmpBuilder = webDriverBuilder.forBrowser(type)
@@ -82,14 +86,15 @@ class WhatsApp {
             return tmpBuilder.setSafariOptions(new safari.Options().headless().windowSize(screen))
         }
       } else {
+        // return tmpBuilder.setFirefoxOptions(new firefox.Options().setProfile(profile))
         return tmpBuilder
       }
     })().build()
     this.browser.get('https://web.whatsapp.com/')
   }
-  async start(onQrChanged) {
+  async start(onQrChanged, onLoggedIn) {
     // wait for qr code
-    await this.browser.wait(Until.elementLocated(By.css('* img')))
+    await this.browser.wait(Until.elementLocated(By.css('* img[alt="Scan me!"]')))
     // // don't remember me
     // let rememberMe = await this.browser.findElement(By.xpath('//input[@name="rememberMe"]'))
     // if (rememberMe) {
@@ -103,8 +108,8 @@ class WhatsApp {
     // wait for login
     const getQrRelatedElement = async () => {
       // wait for qr code
-      await this.browser.wait(Until.elementLocated(By.css('* img')))
-      let r1 = await this.browser.findElement(By.css('* img'))
+      await this.browser.wait(Until.elementLocated(By.css('* img[alt="Scan me!"]')))
+      let r1 = await this.browser.findElement(By.css('* img[alt="Scan me!"]'))
       let r2 = await r1.findElement(By.xpath('./..'))
       return [r1, r2, await r2.getAttribute('data-ref')]
     }
@@ -122,13 +127,14 @@ class WhatsApp {
           onQrChanged(this.lastQr)
         }
       } catch (e) {
+        if (await this.checkLogin()) break
         [qrImg, imgParent, imgDataRef] = await getQrRelatedElement()
         Sleep.sleep(1)
       }
-      if (await this.checkLogin())
-        break
+      if (await this.checkLogin()) break
     }
     delete this.lastQr
+    onLoggedIn()
     this.isLoggedIn = true
     // wait for side panel
     await this.browser.wait(Until.elementLocated(By.css('#pane-side')))
@@ -143,7 +149,7 @@ class WhatsApp {
         console.info(`Name: ${tmpTabObj.name}, Last Message: ${tmpTabObj.lastMsg}`)
       }
     })
-    this.initApi()
+    await this.initApi()
     this.isReady = true
     // this.browser.close()
   }
@@ -194,27 +200,110 @@ class WhatsApp {
     // prepare members in window object
     let memberNames = ['window.WLAPWAPStore', 'window.WLAPStore']
     let funcNameExtractor = [
-      new RegExp(/Wap:n\(\'"(\w+)"\'\)/),
+      new RegExp(/Wap:[a-z]\(\'"(\w+)"\'\)/),
       new RegExp(/\'"(\w+)"\':function\(e,t,a\)\{\"use strict\";e\.exports=\{AllStarredMsgs:/)
     ]
     await appSrcLoc.reverse().asyncForEach(async ([loc, index]) => {
-      let jsData = await new Promise((resolve, reject) => {
-        Https.get(loc, (res) => {
-          let data = ''
-          res.setEncoding('utf8')
-          res.on('data', (chunk) => {
-            data += chunk
+      function getData() {
+        return new Promise((resolve, reject) => {
+          Https.get(loc, (res) => {
+            let data = ''
+            res.setEncoding('utf8')
+            res.on('data', (chunk) => {
+              data += chunk
+            })
+            res.on('end', () => resolve(data))
           })
-          res.on('end', () => resolve(data))
         })
-      })
-      let funcName = funcNameExtractor[index].exec(jsData)[1]
+      }
+      let funcName = null
+      while (true) {
+        try {
+          let jsData = await getData()
+          funcName = funcNameExtractor[index].exec(jsData)[1]
+          break
+        } catch (e) {
+          console.info('Retrying API initialization')
+          Sleep.msleep(500)
+        }
+      }
       this.browser.executeScript(`${memberNames[index]} = {}`)
       this.browser.executeScript(
         `webpackJsonp([], { ["${funcName}"]: (x, y, z) => ${memberNames[index]} = z(\'"${funcName}"\') }, "${funcName}")`
       )
       // console.info(`Init ${memberNames[index]}`)
     })
+  }
+  async setPresenceHook(phoneNo) {
+    this.browser.executeScript(
+      `
+      if (!window.eventStack) {
+        window.eventStack = {}
+      }
+      window.eventStack['${phoneNo}'] = []
+      `
+    )
+    this.browser.executeScript(
+      `
+      (async () => {
+        let target = await new Promise((resolve) => {
+          let timer = setInterval(async() => {
+            let target = await window.WLAPStore.Presence.find('${phoneNo}@c.us')
+            if (target._events['change:isOnline']) {
+              clearInterval(timer)
+              resolve(target)
+            }
+          }, 2000)
+        })
+        console.log(target)
+        let tmpCallback = target._events['change:isOnline'][0].callback
+        target._events['change:isOnline'][0].callback = (...args) => {
+          console.log(args)
+          if (window.eventStack['${phoneNo}']) {
+            window.eventStack['${phoneNo}'].push([Date.now(), ((args[1])? 1:0)])
+          }
+          return tmpCallback(...args)
+        }
+      })()
+      `
+    )
+  }
+  async getPresenceHistory() {
+    return await this.browser.executeAsyncScript(
+      `
+      let retObj = {}
+      if (window.eventStack) {
+        for (let phoneNo in window.eventStack) {
+          retObj[phoneNo] = []
+          for (let i = 0; i < window.eventStack[phoneNo].length; i++) {
+            retObj[phoneNo].push(window.eventStack[phoneNo].shift())
+          }
+        }
+      }
+      arguments[arguments.length - 1](retObj)
+      `
+    )
+  }
+  async getImage(url) {
+    return await this.browser.executeAsyncScript(
+      `
+      let request = new XMLHttpRequest()
+      // request.onreadystatechange = () => {
+      //   if (request.readyState == 4 && request.status == 200)
+      //   arguments[arguments.length - 1](request.response)
+      // }
+      request.open('GET', '${url}', true)
+      request.responseType = 'blob'
+      request.send(null)
+      request.addEventListener('load', () => {
+        let reader = new FileReader()
+        reader.readAsDataURL(request.response)
+        reader.addEventListener('loadend', () => {
+          arguments[arguments.length - 1](reader.result)
+        })
+      })
+      `
+    )
   }
 }
 
